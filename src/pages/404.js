8cc28ddd081bbd1e504667cf06a94ebfa9383ab1
yakubsiyam <itsyakubsiyam@gmail.com>
2023-07-19 23:36:54 +0600

    Install ant design and explore homePage layout

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <img src="https://static.vecteezy.com/system/resources/previews/006/549/647/original/404-landing-page-free-vector.jpg" />
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/pages/news/index.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+const NewsHomePage = () => {
+  return (
+    <div>
+      <h1>This is news home page</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+};
+
+export default NewsHomePage;
